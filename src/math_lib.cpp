7dc5bde08fa4e19401382c1ede409cf9e14f5950
Tomas Odehnal <xodehn08@stud.fit.vutbr.cz>
2017-04-17 21:05:59 +0200

    math_lib.cpp: Added implementation of functions natural_power and factorial

--- src/math_lib.cpp
+++ src/math_lib.cpp
@@ -28,15 +28,37 @@ double Math_lib::div(double operand1, double operand2) {
 }
 
 double Math_lib::natural_power(double operand, int exp) {
+    if(exp == 0) {
+        if(operand != 0) return 1;
+        else return numeric_limits<double>::quiet_NaN();
+    }
+    if(exp < 0) {
+        throw invalid_argument("Exponent must be bigger or equal to 0.");
+    }
 
+    double result = 1;
+    for(int i=1; i<=exp; i++) {
+        result *= operand;
+    }
+    return result;
 }
 
 double Math_lib::general_sqrt(double base, double exponent) {
 
 }
 
-long Math_lib::factorial(int n) {
-
+unsigned long long Math_lib::factorial(int n) {
+    if(n>20) {
+        throw overflow_error("Overflow of number in factorial!");
+    }
+    if(n < 0) {
+        throw invalid_argument("Number must be bigger or equal to 0.");
+    }
+    unsigned long long factorial = 1;
+    for(int i=1; i<=n; i++) {
+        factorial *= i;
+    }
+    return factorial;
 }
 
 string Math_lib::bin_or(string first, string second) {
